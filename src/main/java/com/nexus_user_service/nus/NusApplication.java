919350c3b78ddbe852b3cfb319b5806d2363346f
Gab-Zag <gab.alves.pereira30@gmail.com>
2026-02-04 19:12:54 -0300

    feat: add base project

---
+++ src/main/java/com/nexus_user_service/nus/NusApplication.java
@@ -0,0 +1,13 @@
+package com.nexus_user_service.nus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexus_user_service/nus/NusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus_user_service.nus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
